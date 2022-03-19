3c0e0c5d0d2ef95244caa4b66e61a912e259db76
RitikaJaiswal1234 <101250816+RitikaJaiswal1234@users.noreply.github.com>
2022-03-18 22:04:55 -0700

    Update PrimeNumber.java

--- numbers/PrimeNumber.java
+++ numbers/PrimeNumber.java
@@ -1,26 +1,30 @@
 package com.sebone.java.basic;
-
 import java.util.Scanner;
-/**Class Name:- PrimeNo
-* Objective:-the class given no is prime or not
-* @author :-Ritika jaiswal
-* Date:-17-03-22
-*/
+  /**Class Name:- PrimeNo
+  * Objective:-the class given no is prime or not
+  * @author :-Ritika jaiswal
+  * Date:-17-03-22
+  */
 public class PrimeNumber { //class PrimeNumber
-	/**Method Name:-checkForPrime
-	* Objective:-return the given no is prime or not
-	* input : integer inputNumber
-	* Date:-17-03-22
-	*/
-    public  boolean checkForPrime(int inputNumber) {
+  /**Method Name:-checkForPrime
+  * Objective:-return the given no is prime or not
+  * input : integer inputNumber
+  * Date:-17-03-22
+  */
+ public  boolean checkForPrime(int inputNumber) {
        for (int i = 2; i<= inputNumber/2; i++) {
          if (inputNumber % i == 0)
              return false;
      }
- 
-        	return true;
+     return true;
    }
-	public static void main(String[] args) {
+  /**
+  * Method Name : main Method
+  * Objective : driver method
+  * input : String Array
+  * @param : one parameter
+  */
+public static void main(String[] args) {
     boolean answer;
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter a number :");
