1289460c388b26fac0fc212687d98bdd7b340897
RitikaJaiswal1234 <101250816+RitikaJaiswal1234@users.noreply.github.com>
2022-03-18 21:50:11 -0700

    Update CountDigit.java

--- numbers/CountDigit.java
+++ numbers/CountDigit.java
@@ -1,41 +1,40 @@
 package com.sebone.java.basic;
-
-
-
 import java.util.Scanner;
-/**
-* Class Name : CountDigit
-* Objective : This class gives the number of digits present in given input
-* @author : Ritika jaiswal
-* Date : 17/03/2022
-*/
+     /**
+     * Class Name : CountDigit
+     * Objective : This class gives the number of digits present in given input
+     * @author : Ritika jaiswal
+     * Date : 17/03/2022
+     */
 public class CountDigit {
-	/**
+     /**
      * Method Name : countofDigit
      * Objective : count digits of number given by user
      * input : integer number
      * return : returns integer as count
      * Date : 17/03/2022
-    */  
-
-
+     */  
 	public int countofDigit(int number) {
-		int count=0;
-		while(number>0){
-			number/=10;
-			count++;
-		}
-		return count;
-    }
-			 
+	       int count=0;
+		while(number>0) {
+	          number/=10;
+	          count++;
+	       }
+	return count;
+    }		 
+     /**
+     * Method Name : main Method
+     * Objective : driver method
+     * input : String Array
+     * @param : one parameter
+     */
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number : ");
-        int number = scanner.nextInt();
-        CountDigit countDigit=new CountDigit();
-        int answer=countDigit.countofDigit(number);
-        System.out.println("count of a given number is = "+answer);
-        scanner.close();
-	}
-
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter number : ");
+         int number = scanner.nextInt();
+         CountDigit countDigit=new CountDigit();
+         int answer=countDigit.countofDigit(number);
+         System.out.println("count of a given number is = "+answer);
+         scanner.close();
+	} 
 }
